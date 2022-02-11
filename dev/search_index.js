e3880db8b97fd641e423ad74f8834648d0705d4b
Documenter.jl <documenter@juliadocs.github.io>
2022-02-11 13:45:32 +0000

    build based on 5ee5834

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HGF","category":"page"},{"location":"#HGF","page":"Home","title":"HGF","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HGF.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HGF]","category":"page"}]
+}
