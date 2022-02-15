8ad33f6c65423f40406ac16b67e3f0d3fae6bac5
JacopoC7 <50409119+JacopoC7@users.noreply.github.com>
2022-02-15 10:42:18 +0100

    Multiple dispatch test

--- src/HGF.jl
+++ src/HGF.jl
@@ -1,12 +1,26 @@
 module HGF
 
-
 #Dummy function 
-function dummy_function(dummy_in)
+function dummy_function(dummy_in::Int64)
     #Just multiplies input by 2
     dummy_out = dummy_in * 2
     return dummy_out
 end
+
+function dummy_funtion(dummy_in::BitArray)
+    if dummy_in[1]==true
+        dummy_out = 1
+    else
+        dummy_out = 2
+    end
+    return dummy_out
+end
+
+function dummy_funtion(dummy_in::Vector{Float64})
+    dummy_out = sum(dummy_in)
+    return dummy_out
+end
+
 
 export dummy_function
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "Dummy tests" begin
     @test dummy_function(2) == 4
+    @test dummy_function([1,0,0,1]) == 1
+    @test dummy_function([1,2,3,4]) == 10
 end
+
+
