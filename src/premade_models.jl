69ae8cdc17daf83d1c38b0280e4409b6fce1e9e9
Peter Thestrup Waade <peter@waade.net>
2022-03-22 16:27:50 +0100

    cleaned up premade_models a bit

--- src/premade_models.jl
+++ src/premade_models.jl
@@ -7,94 +7,86 @@
 
 Function for initializing the structure of an HGF model.
 """
-function premade_HGF(
-    model_name::String,
-    params_list,
-    starting_state_list,
-)
+function premade_HGF(model_name::String, params_list, starting_state_list)
+    #Return a list of models
+    
+    #The standard 2 level HGF
     if model_name == "Standard2level"
-        standard_function_2level(
-            params_list,
-            starting_state_list,
-        )
+        standard_function_2level(params_list, starting_state_list)
+    #The JGET HGF model
     elseif model_name == "Standard3level"
-        standard_function_3level(
-            params_list,
-            starting_state_list,
-        )
-    else 
-        return "error" #just for me I will change it later
+        standard_function_3level(params_list, starting_state_list)
+    #Raise an error if the string doesn't match any model
+    else
+        return "error"
     end
 end
 
 
+"""
+    standard_function_2level(params_list, starting_state_list)
 
-function standard_function_2level(
-    params_list,
-    starting_state_list,
-)
-    input_nodes = [(
-        name = "x_in1",
-        params = (; evolution_rate = params_list.evolution_rate_in1),
-    )
-    ]
+The standard 2 level HGF. It has a continous input node U, with a single value parent x1, which in turn has a single volatility parent x2.
+"""
+function standard_function_2level(params_list, starting_state_list)
+    input_nodes =
+        [(name = "x_in1", params = (; evolution_rate = params_list.evolution_rate_in1))]
     state_nodes = [
-        (name = "x_1", params = (; evolution_rate = params_list.evolution_rate_1), starting_state = starting_state_list.starting_state_1),
+        (
+            name = "x_1",
+            params = (; evolution_rate = params_list.evolution_rate_1),
+            starting_state = starting_state_list.starting_state_1,
+        ),
     ]
     child_parent_relations = [
         (
             child_node = "x_in1",
             value_parents = Dict("x_1" => params_list.coupling_1_in1),
-            volatility_parents = Dict()
+            volatility_parents = Dict(),
         ),
     ]
     default_params = (
         params = (; evolution_rate = 3),
-        starting_state = (;
-            posterior_mean = 1,
-            posterior_precision = 1,
-        ),
+        starting_state = (; posterior_mean = 1, posterior_precision = 1),
     )
-    Standard2levelHGF = HGF.init_HGF(
-            default_params,
-            input_nodes,
-            state_nodes,
-            child_parent_relations,
-    )
+    Standard2levelHGF =
+        HGF.init_HGF(default_params, input_nodes, state_nodes, child_parent_relations)
     return Standard2levelHGF
 end
 
-function standard_function_3level(
-    params_list,
-    starting_state_list,
-)
-    input_nodes = [
-        (name = "x_in1",
-        params = (; evolution_rate = params_list.evolution_rate_in1),)
-    ]
+
+"""
+    standard_function_3level(params_list, starting_state_list)
+
+The JGET model.
+"""
+function standard_function_3level(params_list, starting_state_list)
+    input_nodes =
+        [(name = "x_in1", params = (; evolution_rate = params_list.evolution_rate_in1))]
     state_nodes = [
-        (name = "x_1", params = (; evolution_rate = params_list.evolution_rate_1), starting_state = starting_state_list.starting_state_1),
-        (name = "x_2", params = (; evolution_rate = params_list.evolution_rate_2), starting_state = starting_state_list.starting_state_2),
-        ]
+        (
+            name = "x_1",
+            params = (; evolution_rate = params_list.evolution_rate_1),
+            starting_state = starting_state_list.starting_state_1,
+        ),
+        (
+            name = "x_2",
+            params = (; evolution_rate = params_list.evolution_rate_2),
+            starting_state = starting_state_list.starting_state_2,
+        ),
+    ]
     child_parent_relations = [
         (
             child_node = "x_in1",
             value_parents = Dict("x_1" => params_list.coupling_1_in1),
-            volatility_parents = Dict("x_2" => params_list.coupling_2_in1)
+            volatility_parents = Dict("x_2" => params_list.coupling_2_in1),
         ),
     ]
     default_params = (
         params = (; evolution_rate = 3),
-        starting_state = (;
-            posterior_mean = 1,
-            posterior_precision = 1,
-        ),
+        starting_state = (; posterior_mean = 1, posterior_precision = 1),
     )
-    Standard3levelHGF = HGF.init_HGF(
-            default_params,
-            input_nodes,
-            state_nodes,
-            child_parent_relations,
-    )
+    Standard3levelHGF =
+        HGF.init_HGF(default_params, input_nodes, state_nodes, child_parent_relations)
     return Standard3levelHGF
 end
