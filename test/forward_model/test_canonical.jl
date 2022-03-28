1ed2793f51fc7d64186a405088b60df4907c5580
JacopoC7 <50409119+JacopoC7@users.noreply.github.com>
2022-03-28 15:55:51 +0200

    benchmark tests

--- test/forward_model/test_canonical.jl
+++ test/forward_model/test_canonical.jl
@@ -1,4 +1,21 @@
 using HGF
 using Test
+using CSV
+#using DataFrames
+input=Array{AbstractFloat}(undef,1)
+open("data\\usdchf.dat") do f
+    for ln in eachline(f)
+        push!(input,parse(Float64, ln))
+    end
+end
+#popfirst!(input)
+length(input)
+params_list =
+    (; u_evolution_rate = log(1e-4), x1_evolution_rate = -13.0, x2_evolution_rate=-2.0, x1_x2_coupling_strength = 1)
+starting_state_list =
+    (; x1_posterior_mean = 1.04, x1_posterior_precision = 1e4, x2_posterior_mean = 1.0, x2_posterior_precision=1,)
 
-data = open("usdchf.dat")
+my_hgf = HGF.premade_HGF("continuous_2level", params_list, starting_state_list)
+benchmark=CSV.read("data\\stdhgf.csv", DataFrame)
+benchmark[!,"X1_mean"]
+benchmark_testing(my_hgf,input,benchmark)
