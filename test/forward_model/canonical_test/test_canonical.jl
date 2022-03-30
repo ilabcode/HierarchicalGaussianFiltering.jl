5451dda0b3d959714979292b2bbc981d5ddf4f95
JacopoC7 <50409119+JacopoC7@users.noreply.github.com>
2022-03-30 14:01:58 +0200

    canonical test

--- test/forward_model/canonical_test/test_canonical.jl
+++ test/forward_model/canonical_test/test_canonical.jl
@@ -3,9 +3,8 @@ using Test
 using CSV
 using DataFrames
 include("test_canonical_helper.jl")
-
 input=Float64[]
-open("data\\usdchf.dat") do f
+open("forward_model//canonical_test//data//canonical_input_trajectory.dat") do f
     for ln in eachline(f)
         push!(input,parse(Float64, ln))
     end
@@ -18,8 +17,10 @@ starting_state_list =
     (; x1_posterior_mean = 1.04, x1_posterior_precision = 1e4, x2_posterior_mean = 1.0, x2_posterior_precision=10,)
 
 my_hgf = HGF.premade_HGF("continuous_2level", params_list, starting_state_list);
-benchmark=CSV.read("data\\stdhgf.csv", DataFrame)
+benchmark=CSV.read("forward_model//canonical_test//data//canonical_python_trajectory.csv", DataFrame)
 #benchmark[!,"X1_mean"]
 benchmark_testing(my_hgf,input,benchmark)
 my_hgf = HGF.premade_HGF("continuous_2level", params_list, starting_state_list);
 benchmark_printing(my_hgf,input,benchmark)
+my_hgf = HGF.premade_HGF("continuous_2level", params_list, starting_state_list);
+benchmark_testing_all(my_hgf,input,benchmark,10)
