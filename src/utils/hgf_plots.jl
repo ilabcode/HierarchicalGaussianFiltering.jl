2c8716a15fa209c9d641dbbd46740ae0e595a455
JacopoC7 <50409119+JacopoC7@users.noreply.github.com>
2022-04-06 15:33:30 +0200

    minor

--- src/utils/hgf_plots.jl
+++ src/utils/hgf_plots.jl
@@ -1,10 +1,14 @@
 using RecipesBase
-@userplot Trajectory_Plot
-@recipe function f(pl::Trajectory_Plot)
+@userplot HGF_Trajectory_Plot
+@recipe function f(pl::HGF_Trajectory_Plot)
     hgf = pl.args[1]
     node = pl.args[2]
     if node in keys(hgf.state_nodes)
-        property = pl.args[3]
+        if length(pl.args)<=2
+            property = "posterior"
+        else
+            property = pl.args[3]
+        end
         if property in ["posterior", "prediction"]
             mean = getproperty(hgf.state_nodes[node].history, Symbol(property * "_mean"))
             precision = getproperty(hgf.state_nodes[node].history, Symbol(property * "_precision"))
@@ -14,20 +18,35 @@ using RecipesBase
                 c:="red"
                 mean
             end
-        elseif property in ["value_prediction_error", "volatility_prediction_error"]
-            error = getproperty(hgf.state_nodes[node].history, Symbol(property))
+        elseif property in ["value_prediction_error", "volatility_prediction_error","posterior_precision","prediction_precision","posterior_mean","prediction_mean", "prediction_volatility", "auxiliary_prediction_precision"]
+            value = getproperty(hgf.state_nodes[node].history, Symbol(property))
             @series begin
-                error
+                value
             end
         else
-            print("error")
+            error(property*" is not a supported property for state nodes.")
         end
-    end
-    if node in keys(hgf.input_nodes)
-        input = hgf.input_nodes[node].history.input_value
-        @series begin
-            seriestype := :scatter
-            input
+    elseif node in keys(hgf.input_nodes)
+        if length(pl.args)<=2
+            property = "input_value"
+        else
+            property = pl.args[3]
         end
+        if property in ["input_value"]
+            input = getproperty(hgf.input_nodes[node].history,Symbol(property))
+            @series begin
+                seriestype := :scatter
+                input
+            end
+        elseif property in ["value_prediction_error", "volatility_prediction_error", "prediction_precision","prediction_volatility", "auxiliary_prediction_precision"]
+            input = getproperty(hgf.input_nodes[node].history,Symbol(property))
+            @series begin
+                input
+            end
+        else
+            error(property*" is not an input node property")
+        end
+    else
+        error(node*" is not an HGF node")
     end
 end
