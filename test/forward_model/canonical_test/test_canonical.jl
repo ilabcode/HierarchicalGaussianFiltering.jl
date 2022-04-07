700e9e9ccb5aad4f84c1fa619455d8f53999ce56
JacopoC7 <50409119+JacopoC7@users.noreply.github.com>
2022-04-07 13:53:29 +0200

    Cleaned and commented

--- test/forward_model/canonical_test/test_canonical.jl
+++ test/forward_model/canonical_test/test_canonical.jl
@@ -2,26 +2,41 @@ using HGF
 using Test
 using CSV
 using DataFrames
+
+# file including the testing functions
+
 include("test_canonical_helper.jl")
-input=Float64[]
+
+#importing the canonical input data
+
+input = Float64[]
 open("forward_model//canonical_test//data//canonical_input_trajectory.dat") do f
     for ln in eachline(f)
-        push!(input,parse(Float64, ln))
+        push!(input, parse(Float64, ln))
     end
 end
-input[1]
-length(input)
-params_list =
-    (; u_evolution_rate = log(1e-4), x1_evolution_rate = -13.0, x2_evolution_rate=-2.0, x1_x2_coupling_strength = 1)
-starting_state_list =
-    (; x1_posterior_mean = 1.04, x1_posterior_precision = 1e4, x2_posterior_mean = 1.0, x2_posterior_precision=10,)
 
+#setting up the hgf
+
+params_list =
+    (; u_evolution_rate=log(1e-4), x1_evolution_rate=-13.0, x2_evolution_rate=-2.0, x1_x2_coupling_strength=1)
+starting_state_list =
+    (; x1_posterior_mean=1.04, x1_posterior_precision=1e4, x2_posterior_mean=1.0, x2_posterior_precision=10)
 my_hgf = HGF.premade_hgf("continuous_2level", params_list, starting_state_list);
-benchmark=CSV.read("forward_model//canonical_test//data//canonical_python_trajectory.csv", DataFrame)
-#benchmark[!,"X1_mean"]
-benchmark_testing(my_hgf,input,benchmark,"x1_mean",10)
+
+#importing the target dataframe to compare to
+
+target = CSV.read("forward_model//canonical_test//data//canonical_python_trajectory.csv", DataFrame)
+
+
+#checking for first wrong value in a feature
+canonical_test_index(my_hgf, input, target, "x1_mean", 10)
+
+#resetting the hgf and printing all the failed comparisons
 my_hgf = HGF.premade_hgf("continuous_2level", params_list, starting_state_list);
-benchmark_printing(my_hgf,input,benchmark, "x1_mean",10)
+canonical_test_print_error(my_hgf, input, target, "x1_mean", 10)
+
+#resetting the hgf and testing all the properties in features
 my_hgf = HGF.premade_hgf("continuous_2level", params_list, starting_state_list);
-features = ["x1_mean","x1_precision", "x2_mean","x2_precision"]
-benchmark_testing_all(my_hgf,input,benchmark, features, 10)
+features = ["x1_mean", "x1_precision", "x2_mean", "x2_precision"]
+canonical_test(my_hgf, input, target, features, 10)
--- test/forward_model/canonical_test/test_canonical_helper.jl
+++ test/forward_model/canonical_test/test_canonical_helper.jl
@@ -1,26 +1,29 @@
-#single feature helper test functions
 using HGF
 using DataFrames
 
-function benchmark_printing(HGF_struct::HGF.HGFStruct,input::Vector{Float64},benchmark::DataFrame,feature::String,rounding::Int)
-    feat = split(feature,"_")
-    sym = Symbol("posterior_"*feat[2])
-    failed_tests=[]
+# Testing function: print all the values different from the target value for a selected feature
+
+function canonical_test_print_error(HGF_struct::HGF.HGFStruct, input::Vector{Float64}, target::DataFrame, feature::String, rounding::Int)
+    feat = split(feature, "_")
+    sym = Symbol("posterior_" * feat[2])
+    failed_tests = []
     for i in range(1, length(input))
-        test = round(getproperty(HGF_struct.state_nodes[feat[1]].state,sym),digits=rounding) == round(benchmark[!,feature][i],digits=rounding)
+        test = round(getproperty(HGF_struct.state_nodes[feat[1]].state, sym), digits=rounding) == round(target[!, feature][i], digits=rounding)
         if !test
-            push!(failed_tests,(getproperty(HGF_struct.state_nodes[feat[1]].state,sym),benchmark[!,feature][i]))
+            push!(failed_tests, (getproperty(HGF_struct.state_nodes[feat[1]].state, sym), target[!, feature][i]))
         end
         HGF.update_hgf!(HGF_struct, input[i])
     end
     return failed_tests
 end
 
-function benchmark_testing(HGF_struct::HGF.HGFStruct,input::Vector{Float64},benchmark::DataFrame,feature::String,rounding::Int)
-    feat = split(feature,"_")
-    sym = Symbol("posterior_"*feat[2])    
+# Testing function: print the index of the first value different from the target value for a selected feature
+
+function canonical_test_index(HGF_struct::HGF.HGFStruct, input::Vector{Float64}, target::DataFrame, feature::String, rounding::Int)
+    feat = split(feature, "_")
+    sym = Symbol("posterior_" * feat[2])
     for i in range(1, length(input))
-        test = round(getproperty(HGF_struct.state_nodes[feat[1]].state,sym),digits=rounding) == round(benchmark[!,feature][i],digits=rounding)
+        test = round(getproperty(HGF_struct.state_nodes[feat[1]].state, sym), digits=rounding) == round(target[!, feature][i], digits=rounding)
         HGF.update_hgf!(HGF_struct, input[i])
         if !test
             return i
@@ -29,28 +32,28 @@ function benchmark_testing(HGF_struct::HGF.HGFStruct,input::Vector{Float64},benc
     return "success!!!"
 end
 
-#multi feature test function
+# Testing function: gives the number of failed test against a target dataframe for the selected features
 
-function benchmark_testing_all(HGF_struct::HGF.HGFStruct,input::Vector{Float64},benchmark::DataFrame,features::Vector{String},rounding::Int)
-    failed_tests=Dict()
+function canonical_test(HGF_struct::HGF.HGFStruct, input::Vector{Float64}, target::DataFrame, features::Vector{String}, rounding::Int)
+    failed_tests = Dict()
     for feature in features
-        failed_tests[feature]=[]
+        failed_tests[feature] = []
     end
     for i in range(1, length(input))
         for feature in features
-            feat=split(feature,"_")        
-            test = round(getproperty(HGF_struct.state_nodes[feat[1]].state,Symbol("posterior_"*feat[2])),digits=rounding) == round(benchmark[!,feature][i],digits=rounding)
+            feat = split(feature, "_")
+            test = round(getproperty(HGF_struct.state_nodes[feat[1]].state, Symbol("posterior_" * feat[2])), digits=rounding) == round(target[!, feature][i], digits=rounding)
             if !test
-                push!(failed_tests[feature],(getproperty(HGF_struct.state_nodes[feat[1]].state,Symbol("posterior_"*feat[2])),benchmark[!,feature][i]))
+                push!(failed_tests[feature], (getproperty(HGF_struct.state_nodes[feat[1]].state, Symbol("posterior_" * feat[2])), target[!, feature][i]))
             end
         end
-    HGF.update_hgf!(HGF_struct, input[i])
+        HGF.update_hgf!(HGF_struct, input[i])
     end
     for feature in features
-        if length(failed_tests[feature])==0
-            println(feature,": Test passed")
+        if length(failed_tests[feature]) == 0
+            println(feature, ": Test passed")
         else
-            println(feature,": ",length(failed_tests[feature])," tests failed")
+            println(feature, ": ", length(failed_tests[feature]), " tests failed")
         end
     end
 end
