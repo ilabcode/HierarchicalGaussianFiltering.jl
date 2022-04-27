f5bd91018a9400f4287cf17f700a4d5a1a1bd3b7
Peter Thestrup Waade <peter@waade.net>
2022-04-27 16:16:32 +0200

    minor

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,13 @@ using CSV
 using DataFrames
 
 # Test that the HGF gives canonical outputs
-include("HGF_perception_model/test_canonical.jl")
+include("hgf_perception_model/test_canonical.jl")
 
 # Test initialization
-include("HGF_perception_model/test_initialization.jl")
+include("hgf_perception_model/test_initialization.jl")
 
 # Test premade models
-include("HGF_perception_model/test_premade_models.jl")
+include("hgf_perception_model/test_premade_models.jl")
 
 # Test update equations
 # include("HGF_perception_model/test_update_equations.jl")
