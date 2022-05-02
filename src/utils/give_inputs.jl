58d1dc4848a8a7d50eece1268d4defb44ad4c217
Peter Thestrup Waade <peter@waade.net>
2022-05-02 15:13:42 +0200

    minor

--- src/utils/give_inputs.jl
+++ src/utils/give_inputs.jl
@@ -1,4 +1,21 @@
 """
+    input(HGF_struct::HGFStruct, inputs::Number)
+
+Function for inputting multiple observations to an HGF. Input is a single value.
+"""
+function give_inputs!(HGF::HGFStruct, inputs::Number)
+
+    ### Checks ###
+    #No checks
+
+    ### Input data ###
+    #Input the value to the HGF
+    update_hgf!(HGF, inputs)
+
+    return nothing
+end
+
+"""
     input(HGF_struct::HGFStruct, inputs::Array)
 
 Function for inputting multiple observations to an HGF. Input is structured as an array, with one column per input node and one row per timestep.
@@ -29,7 +46,7 @@ end
 
 
 """
-    input(HGF_struct::HGFStruct, inputs)
+    input(HGF_struct::HGFStruct, inputs::Dict{String,Vector})
 
 Function for inputting multiple observations to an HGF. Input is structured as a dictionary with a vector for each input node.
 """
@@ -80,9 +97,26 @@ end
 
 
 
+function give_inputs!(agent::AgentStruct, input::Number)
+
+    ### Input data ###
+    #Take input
+
+    #Get the action distribution
+    distribution = agent.action_model(agent, input)
+
+    #Sample the action from the distribution
+    agent.state["action"] = rand(distribution, 1)[1]
+
+    #Record the action
+    push!(agent.history["action"], agent.state["action"])
+
+    return distribution
+end
+
 
 """
-    input(action_struct::AgentStruct, inputs::Array)
+    input(agent::AgentStruct, inputs::Array)
 
 Function for inputting multiple observations to an action model. Input is structured as an array, with one column per input node and one row per timestep.
 """
@@ -117,40 +151,3 @@ function give_inputs!(agent::AgentStruct, inputs::Array)
     return nothing
 end
 
-# function give_inputs!(action_struct::AgentStruct, inputs::Number)
-
-#     ### Checks ###
-#     #If number of column in input is diffferent from amount of input nodes
-#     if size(inputs, 2) != length(action_struct.perceptual_struct.input_nodes)
-#         #Raise an error
-#         throw(ArgumentError("the number of columns in the input is different from the number of input nodes in the model"))
-#     end
-
-#     ### Input data ###
-#     #Take each row in the array
-#     for rownr in 1:size(inputs, 1)
-#         #Input it to the model and record the action
-#         action_struct.state["action"] = action_struct.action_model(action_struct, inputs[rownr,:])[1]
-#         push!(action_struct.history["action"], action_struct.state["action"])
-#     end
-
-#     return nothing
-# end
-
-
-function give_inputs!(agent::AgentStruct, input::Number)
-
-    ### Input data ###
-    #Take input
-
-    #Get the action distribution
-    distribution = agent.action_model(agent, input)
-
-    #Sample the action from the distribution
-    agent.state["action"] = rand(distribution, 1)[1]
-
-    #Record the action
-    push!(agent.history["action"], agent.state["action"])
-
-    return distribution
-end
