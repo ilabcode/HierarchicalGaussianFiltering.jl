8b88f7268f9e6a05bfd2bee7ac7dd842a5bb2f11
Peter Thestrup Waade <peter@waade.net>
2022-05-02 15:11:15 +0200

    Fixed give_input for dictionaries

--- src/utils/give_inputs.jl
+++ src/utils/give_inputs.jl
@@ -46,16 +46,32 @@ function give_inputs!(HGF::HGFStruct, inputs::Dict{String,Vector})
         )
     end
 
-    #If all input vectors are not of the same length
-    for input_set in input
+    #Make empty list for populating
+    lengths_list = []
+    #Go through each input
+    for input_list in values(inputs)
+        #Add the length of the input to the list
+        push!(lengths_list, length(input_list))
+    end
 
+    #If all lengths are not equal
+    if .!all(y -> y == lengths_list[1], lengths_list)
+        #Raise an error
+        throw(ArgumentError("the lists of inputs to each node are not of the same length"))
     end
 
     ### Input data ###
+    #Create empty dictionary for a single input
+    input = Dict()
 
-    #
-
-    for input in inputs
+    #Go through a sequence as long as the length of the first entry
+    for input_nr = 1:length(first(inputs)[2])
+        #Go through each node in the input
+        for input_node in keys(inputs)
+            #Save the input from the corresponding input number
+            input[input_node] = inputs[input_node][input_nr]
+        end
+        #And input it to the HGF
         update_hgf!(HGF, input)
     end
 
@@ -123,18 +139,18 @@ end
 
 
 function give_inputs!(agent::AgentStruct, input::Number)
-    
-        ### Input data ###
-        #Take input
-        
-        #Get the action distribution
-        distribution = agent.action_model(agent, input)
 
-        #Sample the action from the distribution
-        agent.state["action"] = rand(distribution, 1)[1]
+    ### Input data ###
+    #Take input
 
-        #Record the action
-        push!(agent.history["action"], agent.state["action"])
-    
-        return distribution
+    #Get the action distribution
+    distribution = agent.action_model(agent, input)
+
+    #Sample the action from the distribution
+    agent.state["action"] = rand(distribution, 1)[1]
+
+    #Record the action
+    push!(agent.history["action"], agent.state["action"])
+
+    return distribution
 end
