b41e9eb24cd67ba7570f7ebad3c76ff5facba87e
Peter Thestrup Waade <peter@waade.net>
2022-05-03 09:49:58 +0200

    updated give_inputs!

--- src/model_fitting/fit_model.jl
+++ src/model_fitting/fit_model.jl
@@ -1,5 +1,13 @@
 using Turing
-function fit_model(agent::AgentStruct,inputs::Vector{Float64},responses::Vector{Float64},params_priors_list=[]::Vector{Tuple{String, Distribution{Univariate,Continuous}}},fixed_params_list=[]::Vector{Tuple{String, Real}}, sampler=NUTS(),iterations=1000)
+function fit_model(
+    agent::AgentStruct,
+    inputs::Vector{Float64},
+    responses::Vector{Float64},
+    params_priors_list = []::Vector{Tuple{String,Distribution{Univariate,Continuous}}},
+    fixed_params_list = []::Vector{Tuple{String,Real}},
+    sampler = NUTS(),
+    iterations = 1000,
+)
     old_params = get_params(agent)
     change_params(agent::AgentStruct, fixed_params_list)
     @model function fit_hgf(y::Vector{Float64})
@@ -10,15 +18,15 @@ function fit_model(agent::AgentStruct,inputs::Vector{Float64},responses::Vector{
         reset!(agent)
         params_list = []
         for i in params
-            push!(params_list,(i[1],i[2]))
+            push!(params_list, (i[1], i[2]))
         end
         change_params(agent::AgentStruct, params_list)
 
-        for i in range(1,length(responses))
+        for i in range(1, length(responses))
             y[i] ~ agent.action_model(agent, inputs[i])
         end
     end
-    chain=sample(fit_hgf(responses), sampler,iterations)
+    chain = sample(fit_hgf(responses), sampler, iterations)
     change_params(agent, old_params)
     reset!(agent)
     return chain
