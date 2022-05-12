84c080c15ff141809baa11b88520880c7971cfb6
JacopoC7 <50409119+JacopoC7@users.noreply.github.com>
2022-05-12 16:06:41 +0200

    added get_params option for chains

--- src/utils/get_params.jl
+++ src/utils/get_params.jl
@@ -25,3 +25,13 @@ function get_params(agent::AgentStruct)
     end
     return params_list
 end
+
+
+function get_params(chain::Chains)
+    df = describe(chain)[2]
+    params_list = (;)
+    for i in 1:getproperty(df,:nrows)
+        params_list = merge(params_list, (df.:nt.parameters[i] => getproperty(df.:nt,Symbol("50.0%"))[i],))
+    end
+    return params_list
+end
--- tutorials/canonical_complete.jl
+++ tutorials/canonical_complete.jl
@@ -114,7 +114,7 @@ u_x1_coupling_strenght = 1.0,
 x1_x2_coupling_strenght = 1.0,
 action_noise =0.01,
 x2_posterior_mean = 1.,
-#x1_posterior_precision = first20_variance
+x1_posterior_precision = first20_variance
 )
 
 params_prior_list = (
@@ -122,10 +122,37 @@ u_evolution_rate = Normal(log(first20_variance),2),
 x1_evolution_rate = Normal(log(first20_variance),4),
 x2_evolution_rate = Normal(-4,4),
 x1_posterior_mean = Normal(first_input,sqrt(first20_variance)),
-x1_posterior_precision = Truncated(LogNormal(HGF.lognormal_params(1/first20_variance,1).mean,HGF.lognormal_params(1/first20_variance,1).std),0,2/first20_variance),
+#x1_posterior_precision = Truncated(LogNormal(HGF.lognormal_params(1/first20_variance,1).mean,HGF.lognormal_params(1/first20_variance,1).std),0,2/first20_variance),
 x2_posterior_precision = LogNormal(HGF.lognormal_params(10,1).mean,HGF.lognormal_params(10,1).std),
 )
 
 @time chain2=HGF.fit_model(my_agent,inputs,responses,params_prior_list,fixed_params_list,NUTS(),1000)
 
+fitted_params=HGF.get_params(chain2)
 
+HGF.set_params(my_agent,fitted_params)
+HGF.reset!(my_agent)
+
+responses=HGF.give_inputs!(my_agent,inputs)
+
+hgf_trajectory_plot(my_agent, "u",
+size=(1300,500),
+xlims = (0,615),
+markerstrokecolor = :auto,
+markersize=3,
+markercolor = "green2")
+hgf_trajectory_plot!(my_agent, "x1", "posterior_mean",
+color="red",
+linewidth=1.5)
+hgf_trajectory_plot!(my_agent, "action",
+size=(1300,500),
+xlims = (0,614),
+markerstrokecolor = :auto,
+markersize=3,
+markercolor = "orange")
+
+hgf_trajectory_plot(my_agent, "x2",
+color="blue",
+size=(1300,500),
+xlims = (0,615),
+title =L"Posterior\:expectation\,of\,x_{2}")
