f9c2a49bb48adbc8431f69428f3cde26f4ed8e5f
Peter Thestrup Waade <peter@waade.net>
2022-07-15 20:56:51 +0200

    minor

--- src/action_model/init_agent.jl
+++ src/action_model/init_agent.jl
@@ -1,28 +1,28 @@
 function init_agent(action_model::Function, perception_struct, params, states, settings)
 
     #Create action model struct
-    action_struct =
+    agent =
         HGF.AgentStruct(action_model = action_model, perception_struct = perception_struct)
 
     #For each specified parameter
     for param in params
         #Add it and its value to the parameter field
-        action_struct.params[param[1]] = param[2]
+        agent.params[param[1]] = param[2]
     end
 
     #For each specified state
     for state in states
         #Add it to the state field
-        action_struct.state[state[1]] = state[2]
+        agent.state[state[1]] = state[2]
         #And put it in the history
-        action_struct.history[state[1]] = [state[2]]
+        agent.history[state[1]] = [state[2]]
     end
 
     #For each specified setting
     for setting in settings
         #Add it to the settings field
-        action_struct.settings[setting[1]] = setting[2]
+        agent.settings[setting[1]] = setting[2]
     end
 
-    return action_struct
+    return agent
 end
--- src/action_model/premade_models.jl
+++ src/action_model/premade_models.jl
@@ -57,18 +57,21 @@ end
 
 
 
+"""
+    gaussian_action(agent, input)
 
-#Create the function
-function gaussian_action(action_struct, input)
+Action model which reports a given HGF state with Gaussian noise.
+"""
+function gaussian_action(agent, input)
 
     #Get out settings
-    target_node = action_struct.settings["target_node"]
-    target_state = action_struct.settings["target_state"]
+    target_node = agent.settings["target_node"]
+    target_state = agent.settings["target_state"]
     #Get out parameters
-    action_precision = action_struct.params["action_precision"]
+    action_precision = agent.params["action_precision"]
 
     #Get out the HGF
-    hgf = action_struct.perception_struct
+    hgf = agent.perception_struct
 
     #Update the HGF
     hgf.perception_model(hgf, input)
@@ -84,17 +87,21 @@ function gaussian_action(action_struct, input)
 end
 
 
+"""
+    binary_softmax_action(agent, input)
 
-function binary_softmax_action(action_struct, input)
+Action model which gives a binary action. The action probability is the softmax of a specified state of a node.
+"""
+function binary_softmax_action(agent, input)
 
     #Get out settings
-    target_node = action_struct.settings["target_node"]
-    target_state = action_struct.settings["target_state"]
+    target_node = agent.settings["target_node"]
+    target_state = agent.settings["target_state"]
     #Get out parameters
-    action_precision = action_struct.params["action_precision"]
+    action_precision = agent.params["action_precision"]
 
     #Get out the HGF
-    hgf = action_struct.perception_struct
+    hgf = agent.perception_struct
 
     #Update the HGF
     hgf.perception_model(hgf, input)
@@ -113,16 +120,21 @@ function binary_softmax_action(action_struct, input)
 end
 
 
-function unit_square_sigmoid_action(action_struct, input)
+"""
+    unit_square_sigmoid_action(agent, input)
+
+Action model which gives a binary action. The action probability is the unit square sigmoid of a specified state of a node.
+"""
+function unit_square_sigmoid_action(agent, input)
 
     #Get out settings
-    target_node = action_struct.settings["target_node"]
-    target_state = action_struct.settings["target_state"]
+    target_node = agent.settings["target_node"]
+    target_state = agent.settings["target_state"]
     #Get out parameters
-    action_precision = action_struct.params["action_precision"]
+    action_precision = agent.params["action_precision"]
 
     #Get out the HGF
-    hgf = action_struct.perception_struct
+    hgf = agent.perception_struct
 
     #Update the HGF
     hgf.perception_model(hgf, input)
@@ -141,228 +153,3 @@ function unit_square_sigmoid_action(action_struct, input)
     #Return the action distribution
     return distribution
 end
-
-
-
-
-
-
-
-# """
-#     create_gaussian_action(state::String)
-
-# Function for creating a customized gaussian action action model. Takes a node name and a state as arguments. Outputs a function which reports the chosen state from the chosen node with some noise.
-# """
-# function create_gaussian_action(; state::String = "x1__posterior_mean")
-
-#     #Separate into node and state
-#     node, state = split(state, "__")
-
-#     #Change the state to a symbol
-#     state = Symbol(state)
-
-#     #Evaluate the function definition
-#     eval(quote
-
-#         #Create the function
-#         function gaussian_action(action_struct, input)
-
-#             #Get out parameters
-#             action_precision = action_struct.params["action_precision"]
-
-#             #Get out the HGF
-#             hgf = action_struct.perception_struct
-
-#             #Update the HGF
-#             hgf.perception_model(hgf, input)
-
-#             #Extract the specified state from the specified node
-#             target_state = hgf.state_nodes[$node].state.$state
-
-#             #Create normal distribution with mean of the target value and a standard deviation from parameters
-#             distribution = Distributions.Normal(target_state, 1 / action_precision)
-
-#             #Return the action distribution
-#             return distribution
-#         end
-#     end)
-
-#     #Return the action model
-#     return gaussian_action
-# end
-
-
-# """
-#     create_binary_softmax_action(; state::String = "x1__prediction_mean") 
-
-# Function for creating a customized binary softmax action model. Takes a state as argument. Outputs a function which inputs the state into a softmax, to get the action probability for a Bernoulli distribution.
-# """
-# function create_binary_softmax_action(; state::String = "x1__prediction_mean")
-
-#     #Separate into node and state
-#     node, state = split(state, "__")
-
-#     #Change the state to a symbol
-#     state = Symbol(state)
-
-#     #Evaluate the function definition
-#     eval(quote
-
-#         #Create the function
-#         function binary_softmax_action(action_struct, input)
-
-#             #Get out parameters
-#             action_precision = action_struct.params["action_precision"]
-
-#             #Get out the HGF
-#             hgf = action_struct.perception_struct
-
-#             #Update the HGF
-#             hgf.perception_model(hgf, input)
-
-#             #Extract the specified state from the specified node
-#             target_state = hgf.state_nodes[$node].state.$state
-
-#             #Use sotmax to get the action probability 
-#             action_probability = 1 / (1 + exp(-action_precision * target_state))
-
-#             #Create Bernoulli normal distribution with mean of the target value and a standard deviation from parameters
-#             distribution = Distributions.Bernoulli(action_probability)
-
-#             #Return the action distribution
-#             return distribution
-#         end
-#     end)
-
-#     return binary_softmax_action
-# end
-
-
-# """
-#     create_unit_square_sigmoid_action(; state::String = "x1__prediction_mean") 
-
-# Function for creating a customized binary unit square sigmoid action model. Takes a state as argument. Outputs a function which inputs the state into a unit square sigmoid, to get the action probability for a Bernoulli distribution.
-# """
-# function create_unit_square_sigmoid_action(; state::String = "x1__prediction_mean")
-
-#     #Separate into node and state
-#     node, state = split(state, "__")
-
-#     #Change the state to a symbol
-#     state = Symbol(state)
-
-#     #Evaluate the function definition
-#     eval(
-#         quote
-
-#             #Create the function
-#             function unit_square_sigmoid_action(action_struct, input)
-
-#                 #Get out parameters
-#                 action_precision = action_struct.params["action_precision"]
-
-#                 #Get out the HGF
-#                 hgf = action_struct.perception_struct
-
-#                 #Update the HGF
-#                 hgf.perception_model(hgf, input)
-
-#                 #Extract the specified state from the specified node
-#                 target_state = hgf.state_nodes[$node].state.$state
-
-#                 #Use sotmax to get the action probability 
-#                 action_probability =
-#                     target_state^action_precision /
-#                     (target_state^action_precision + (1 - target_state)^action_precision)
-
-#                 #Create Bernoulli normal distribution with mean of the target value and a standard deviation from parameters
-#                 distribution = Distributions.Bernoulli(action_probability)
-
-#                 #Return the action distribution
-#                 return distribution
-#             end
-#         end,
-#     )
-
-#     return unit_square_sigmoid_action
-# end
-
-
-
-
-
-
-
-
-
-
-
-# """
-#     create_gaussian_action(state::Vector{String})
-
-# Function for creating a customized gaussian action action model. Takes a node name and a string of states as arguments. Outputs a function which reports a linear combination of the chosen states from the chosen node with some noise.
-# """
-# function create_gaussian_action(;
-#     state::Vector{String} = ["x1__posterior_mean", "x2__posterior_mean"],
-# )
-
-#     node, state = split(state, "__")
-
-#     #Change the state to a symbol
-#     state = Symbol(state)
-
-#     #Evaluate the function definition
-#     eval(
-#         quote
-
-#             #Create the function
-#             function gaussian_action(action_struct, input)
-
-#                 #Get out the HGF
-#                 hgf = action_struct.perception_struct
-
-#                 #Update the HGF
-#                 hgf.perception_model(hgf, input)
-
-#                 #Extract the specified state from the specified node
-#                 target_state = hgf.state_nodes[$node].state.$state
-
-#                 #Create normal distribution with mean of the target value and a standard deviation from parameters
-#                 distribution = Distributions.Normal(
-#                     target_state,
-#                     1 / action_struct.params["action_precision"],
-#                 )
-
-#                 #Return the action distribution
-#                 return distribution
-#             end
-#         end,
-#     )
-
-#     #Return the action model
-#     return gaussian_action
-# end
-
-
-# """
-#     gaussian_action(agent::AgentStruct, input)
-
-# Gaussian action action model. Updates the hgf, extracts the posterior mean for x1, and reports it with some noise
-# """
-# function gaussian_action(agent::AgentStruct, input)
-
-#     #Get out the HGF
-#     hgf = agent.perception_struct
-
-#     #Update the HGF
-#     hgf.perception_model(hgf, input)
-
-#     #Extract the posterior belief about x1
-#     μ1 = hgf.state_nodes["x1"].state.posterior_mean
-
-#     #Create normal distribution with mean μ1 and a standard deviation from parameters
-#     distribution = Distributions.Normal(μ1, 1 / agent.params["action_precision"])
-
-#     #Return the action dsitribution
-#     return distribution
-# end
