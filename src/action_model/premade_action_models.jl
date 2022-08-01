e46c617f0b131cd43d5ecbbdc332efeb84ebcf8c
Peter Thestrup Waade <peter@waade.net>
2022-08-01 15:13:40 +0200

    restructuring give_inputs

--- src/action_model/premade_action_models.jl
+++ src/action_model/premade_action_models.jl
@@ -5,8 +5,40 @@ function multiple_actions(agent, input)
     #Extract vector of action models
     action_models = agent.settings["action_models"]
 
+    #Initialize vector for action distributions
+    action_distributions = []
+
+    #Do each action model separately
+    for action_model in action_models
+        #And append them to the vector of action distributions
+        push!(action_distributions, action_model(agent, input))
+    end
+
+    return action_distributions
+end
 
 
+"""
+"""
+function hgf_multiple_actions(agent, input)
+
+    #Update the hgf
+    hgf = agent.perception_struct
+    hgf.perception_model(hgf, input)
+
+    #Extract vector of action models
+    action_models = agent.settings["action_models"]
+
+    #Initialize vector for action distributions
+    action_distributions = []
+
+    #Do each action model separately
+    for action_model in action_models
+        #And append them to the vector of action distributions
+        push!(action_distributions, action_model(agent, input, update_hgf = false))
+    end
+
+    return action_distributions
 end
 
 
--- src/utils/give_inputs.jl
+++ src/utils/give_inputs.jl
@@ -1,12 +1,103 @@
 """
+    give_input!(agent::AgentStruct, input)
+
+Function for giving an input to an AgentStruct.
+"""
+function give_input!(agent::AgentStruct, input)
+
+    ### Input data ###
+    #Run the action model to get the action distribution
+    action_distribution = agent.action_model(agent, input)
+
+    #If a single action distribution is returned
+    if length(action_distribution) == 1
+
+        #Sample an action from the distribution
+        agent.action = rand(action_distribution, 1)[1]
+
+        #If multiple action distributiomns are returned
+    else
+        #Initialize vector for storing actions
+        actions = []
+
+        #For each action distribution
+        for distribution in action_distribution
+            #Sample an action and add it to the vector
+            push!(actions, rand(distribution, 1)[1])
+        end
+
+        #And store it 
+        agent.action = actions
+    end
+
+    #Record the action
+    push!(agent.history["action"], agent.action)
+
+    #Return the action
+    return agent.action
+end
+
+
+"""
+    give_inputs!(agent::AgentStruct, inputs::Vector)
+
+Function for inputting multiple observations to an agent. Input is structured as an vector, with each value being an input.
+"""
+function give_inputs!(agent::AgentStruct, inputs::Vector)
+
+    ### Input data ###
+    #Take each row in the array
+    for rownr = 1:size(inputs, 1)
+
+        #Input that row
+        give_input!(agent, inputs[rownr, :])
+
+    end
+
+    #Return the action trajectory
+    return agent.history["action"]
+end
+
+
+"""
+    give_inputs!(agent::AgentStruct, inputs::Real)
+
+Convenience method for inputting multiple observations to an agent. Input is here just a single value.
+"""
+function give_inputs!(agent::AgentStruct, inputs::Real)
+
+    #Input the single input
+    give_input!(agent, inputs)
+
+    #Return the action trajectory
+    return agent.history["action"]
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
     give_inputs!(hgf::HGFStruct, inputs::Number)
 
 Function for inputting multiple observations to an hgf. Input is a single value.
 """
 function give_inputs!(hgf::HGFStruct, inputs::Number)
-
-    ### Checks ###
-    #No checks
 
     ### Input data ###
     #Input the value to the hgf
@@ -93,109 +184,4 @@ function give_inputs!(hgf::HGFStruct, inputs::Dict{String,Vector})
     end
 
     return nothing
-end
-
-
-"""
-    give_inputs!(agent::AgentStruct, inputs::Number)
-
-Function for giving multiple inputs to an AgentStruct. Here a single number is passed as an input.
-"""
-function give_inputs!(agent::AgentStruct, input::Number)
-
-    ### Checks ###
-    #No checks
-
-    ### Input data ###
-    #Run the action model to get the action distribution
-    distribution = agent.action_model(agent, input)
-
-    #Sample an action from the distribution
-    agent.action = rand(distribution, 1)[1]
-
-    #Record the action
-    push!(agent.history["action"], agent.action)
-
-    #Return the action
-    return agent.action
-end
-
-
-"""
-    give_inputs!(agent::AgentStruct, inputs::Array)
-
-Function for inputting multiple observations to an agent. Input is structured as an array, with one column per input node and one row per timestep.
-"""
-function give_inputs!(agent::AgentStruct, inputs::Array)
-
-    ### Checks ###
-    #No checks
-
-    ### Input data ###
-    #Take each row in the array
-    for rownr = 1:size(inputs, 1)
-
-        #Get the action distribution
-        distribution = agent.action_model(agent, inputs[rownr, :])
-
-        #Sample the action from the distribution
-        agent.action = rand(distribution, 1)[1]
-
-        #Record the action
-        push!(agent.history["action"], agent.action)
-
-    end
-
-    #Return the action trajectory
-    return agent.history["action"]
-end
-
-
-"""
-    give_inputs!(agent::AgentStruct, inputs::Dict{String,Vector})
-
-    Function for inputting multiple observations to an agent. Input is structured as a dictionary with a vector for each input node.
-"""
-function give_inputs!(agent::AgentStruct, inputs::Dict{String,Vector})
-
-    ### Checks ###
-    ## Check that all input lists are the same length
-    #Make empty list for populating
-    lengths_list = []
-    #Go through each input
-    for input_list in values(inputs)
-        #Add the length of the input to the list
-        push!(lengths_list, length(input_list))
-    end
-
-    #If all lengths are not equal
-    if .!all(y -> y == lengths_list[1], lengths_list)
-        #Raise an error
-        throw(ArgumentError("the lists of inputs are not of the same length"))
-    end
-
-    ### Input data ###
-    #Create empty dictionary for a single input
-    input = Dict()
-
-    #Go through a sequence as long as the length of the first entry
-    for input_nr = 1:length(first(inputs)[2])
-        #Go through each node in the input
-        for input_modality in keys(inputs)
-            #Save the input from the corresponding input number
-            input[input_modality] = inputs[input_modality][input_nr]
-        end
-
-        #Run the action model to get the action distribution
-        distribution = agent.action_model(agent, input)
-
-        #Sample an action from the distribution
-        agent.action = rand(distribution, 1)
-
-        #Record the action
-        push!(agent.history["action"], agent.action)
-    end
-
-    #Return the action history
-    return agent.state["action"]
 end
