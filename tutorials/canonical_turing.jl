47f251613b079b1f5150a9547a77485ee58d941c
JacopoC7 <50409119+JacopoC7@users.noreply.github.com>
2022-08-02 10:24:20 +0200

    minor

--- tutorials/canonical_turing.jl
+++ tutorials/canonical_turing.jl
@@ -24,30 +24,30 @@ first_input = inputs[1]
 first20_variance = Turing.Statistics.var(inputs[1:20])
 
 fixed_params_list = (
-    u_x1_coupling_strenght = 1.0,
-    x1_x2_coupling_strenght = 1.0,
-    action_noise = 0.01,
+    u__x1_coupling_strenght = 1.0,
+    x1__x2_coupling_strenght = 1.0,
+    action_precision = 100,
     # x1_evolution_rate = 7.3,
     # u_evolution_rate = -log(9.39e6),
     # x2_evolution_rate =4.2,
-    x2_posterior_mean = 1.0,
+    x2__initial_mean = 1.0,
     # x1_posterior_mean = 1.03,
-    x1_posterior_precision = 1 / (3.2889e-5),
+    x1__initial_precision = 1 / (3.2889e-5),
     # x2_posterior_precision = 1e6,
 )
 
 params_prior_list = (
     # u_x1_coupling_strenght = LogNormal(HGF.lognormal_params(1,0.3).mean,HGF.lognormal_params(1,0.3).std),
-    u_evolution_rate = Normal(log(first20_variance), 2),
-    x1_evolution_rate = Normal(log(first20_variance), 4),
-    x2_evolution_rate = Normal(-4, 4),
-    x1_posterior_mean = Normal(first_input, sqrt(first20_variance)),
+    u__evolution_rate = Normal(log(first20_variance), 2),
+    x1__evolution_rate = Normal(log(first20_variance), 4),
+    x2__evolution_rate = Normal(-4, 4),
+    #x1__initial_mean = Normal(first_input, sqrt(first20_variance)),
     #x2_posterior_mean = Normal(1,0.3),
     # x1_posterior_precision = LogNormal(HGF.lognormal_params(1/first20_variance,1).mean,HGF.lognormal_params(1/first20_variance,1).std),
-    x2_posterior_precision = LogNormal(
-        HGF.lognormal_params(10, 1).mean,
-        HGF.lognormal_params(10, 1).std,
-    ),
+    #x2__initial_precision = LogNormal(
+    #     HGF.lognormal_params(10, 1).mean,
+    #     HGF.lognormal_params(10, 1).std,
+    # ),
 )
 
 #Turing.Statistics.mean(TruncatedNormal(10,5 ,0,Inf))
@@ -55,12 +55,12 @@ params_prior_list = (
 
 params_list = (
     #u_x1_coupling_strenght = 1.15, 
-    u_evolution_rate = -log(9.39e6),
-    x1_evolution_rate = -11.86,
-    x2_evolution_rate = -5.91,
-    x1_posterior_mean = 1.0315,
+    u__evolution_rate = -log(9.39e6),
+    x1__evolution_rate = -11.86,
+    x2__evolution_rate = -5.91,
+    x1__initial_mean = 1.0315,
     #x1_posterior_precision =1/(3.2889e-5),
-    x2_posterior_precision = 1 / 0.0697,
+    x2__initial_precision = 1 / 0.0697,
     #x2_posterior_mean = 1.2
 )
 
@@ -75,11 +75,19 @@ HGF.set_params!(my_agent, fixed_params_list)
 
 #reduced_inputs = inputs[1:200]
 
-responses = HGF.give_inputs!(my_agent, inputs)
+responses = convert.(Float64,HGF.give_inputs!(my_agent, inputs))
 #typeof(responses)
 #chain=HGF.fit_model(my_agent,reduced_input,missing,params_prior_list,fixed_params_list)
 #response = HGF.get_responses(chain)
 @time chain2 =
-    HGF.fit_model(my_agent, inputs, responses, params_prior_list, fixed_params_list)
+    HGF.fit_model(my_agent, inputs, responses, params_prior_list, fixed_params_list, NUTS(), 500, 10)
 #using Plots
 #plot(chain2)
+
+using Plots
+
+HGF.predictive_simulation_plot(my_agent, chain2, "x2__posterior_mean", 1000, inputs)
+
+using StatsPlots
+
+posterior_parameter_plot(chain2,params_prior_list)
