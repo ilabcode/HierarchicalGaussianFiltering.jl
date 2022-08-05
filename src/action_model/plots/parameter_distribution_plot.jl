662a38293766b7498d3301663f34f1ad4ace54ad
Peter Thestrup Waade <peter@waade.net>
2022-08-05 11:13:56 +0200

    cleaned parameter distribution plot

--- src/action_model/plots/parameter_distribution_plot.jl
+++ src/action_model/plots/parameter_distribution_plot.jl
@@ -15,32 +15,21 @@
     plot_height = 300,
 )
 
+    ### Setup ###
     #Get arguments
     chain = pl.args[1]
     params_prior_list = pl.args[2]
 
+    #Get number of subplots
+    n_subplots = length(params_prior_list)
 
-    #Make empty lists for populating with names, titles, medians and interval bounds
-    param_names = []
-    subplot_titles = []
-    prior_median = []
-    posterior_median = []
+    #Specify how to arrange subplots, and their size
+    layout := (n_subplots, 1)
+    size := (plot_width, plot_height * n_subplots)
 
-    prior_inner_interval_lower = []
-    prior_inner_interval_upper = []
-    prior_outer_interval_lower = []
-    prior_outer_interval_upper = []
+    #Initialize counter for plot number
+    plot_number = 0
 
-    posterior_inner_interval_lower = []
-    posterior_inner_interval_upper = []
-    posterior_outer_interval_lower = []
-    posterior_outer_interval_upper = []
-
-    #Initialize empty dictionary for storing quantile values for each parameter
-    param_quantiles = Dict()
-
-
-    ### Get uncertainty interval bar sizes ###
     #Set quantiles that corresponds to specified uncertainty intervals
     interval_quantiles = [
         0.5 - outer_interval * 0.5,
@@ -50,229 +39,262 @@
         0.5 + outer_interval * 0.5,
     ]
 
-
     #For each parameter
     for (param_name, param_prior) in params_prior_list
-
-        #Add the name to a list
-        push!(param_names, param_name)
-
-
 
         #Get posterior from the chain
         param_posterior = Array(chain[:, String(param_name), :])[:]
 
+        ### Get uncertainty interval bar sizes ###
         #Get the quantiles for the prior and posterior
         prior_quantiles = Turing.Statistics.quantile(param_prior, interval_quantiles)
-        posterior_quantiles = Turing.Statistics.quantile(param_posterior, interval_quantiles)
+        posterior_quantiles =
+            Turing.Statistics.quantile(param_posterior, interval_quantiles)
 
-        #Save them in the dictionary
-        param_quantiles[param_name] = (;
-            prior_quantiles = prior_quantiles,
-            posterior_quantiles = posterior_quantiles,
-        )
+        #Get prior median and interval bounds
+        prior_median = prior_quantiles[3]
+        prior_inner_interval_lower = (prior_quantiles[3] - prior_quantiles[2])
+        prior_inner_interval_upper = (prior_quantiles[4] - prior_quantiles[3])
+        prior_outer_interval_lower = (prior_quantiles[3] - prior_quantiles[1])
+        prior_outer_interval_upper = (prior_quantiles[5] - prior_quantiles[3])
+
+        #Get posterior median and interval bounds
+        posterior_median = posterior_quantiles[3]
+        posterior_inner_interval_lower = (posterior_quantiles[3] - posterior_quantiles[2])
+        posterior_inner_interval_upper = (posterior_quantiles[4] - posterior_quantiles[3])
+        posterior_outer_interval_lower = (posterior_quantiles[3] - posterior_quantiles[1])
+        posterior_outer_interval_upper = (posterior_quantiles[5] - posterior_quantiles[3])
 
 
-
+        ### General plotting settings ###
+        #Advance the plot number track one step
+        plot_number = plot_number += 1
 
         #If the user has specified a subplot title
         if param_name in keys(subplot_title_list)
-            #Add the user-specified title to the list
-            push!(subplot_titles, getindex(subplot_title_list, Symbol(param_name)))
-            
-            #Otherwise
+
+            #Use user-specified title
+            title := getindex(subplot_title_list, Symbol(param_name))
+
         else
-            #Use the parameter name as the subplot title
-            push!(subplot_titles, param_name)
+            #Otherwise use the parameter name as the subplot title
+            title := param_name
         end
 
-
-
-        #[prior_median is prior_median]
-        #Add values to lists
-        push!(prior_median, param_quantiles[param_name].prior_quantiles[3])
-        push!(posterior_median, param_quantiles[param_name].posterior_quantiles[3])
-        #Add the sizes fo the error bars (which are the quantiles)
-        push!(prior_inner_interval_lower, (param_quantiles[param_name].prior_quantiles[3] - param_quantiles[param_name].prior_quantiles[2]))
-        push!(prior_inner_interval_upper, (param_quantiles[param_name].prior_quantiles[4] - param_quantiles[param_name].prior_quantiles[3]))
-        push!(prior_outer_interval_lower, (param_quantiles[param_name].prior_quantiles[3] - param_quantiles[param_name].prior_quantiles[1]))
-        push!(prior_outer_interval_upper, (param_quantiles[param_name].prior_quantiles[5] - param_quantiles[param_name].prior_quantiles[3]))
-
-        push!(
-            posterior_inner_interval_lower,
-            (param_quantiles[param_name].posterior_quantiles[3] - param_quantiles[param_name].posterior_quantiles[2]),
-        )
-        push!(
-            posterior_inner_interval_upper,
-            (param_quantiles[param_name].posterior_quantiles[4] - param_quantiles[param_name].posterior_quantiles[3]),
-        )
-        push!(
-            posterior_outer_interval_lower,
-            (param_quantiles[param_name].posterior_quantiles[3] - param_quantiles[param_name].posterior_quantiles[1]),
-        )
-        push!(
-            posterior_outer_interval_upper,
-            (param_quantiles[param_name].posterior_quantiles[5] - param_quantiles[param_name].posterior_quantiles[3]),
-        )
-
-
-
-
-
-    end
-
-
-
-    ### Create plots ###
-
-    #number of subplots
-    l = length(param_names)
-
-    #Specify how to arrange subplots, and their size
-    layout := (l, 1)
-    size := (plot_width, plot_height * l)
-
-    #For each subplot
-    for i = 1:l
-
-        #Set the title
-        title := subplot_titles[i]
-
-        #Aesthetic settings
+        #Remove the y axis
         yshowaxis := false
         yticks := false
 
-        #The font size
+        #Set the font size
         legendfontsize --> 15
+
 
         ### Plot prior and posterior distribution ###
         #if show distributions
         if show_distributions
 
-            #Plot the prior
+            ## Prior distribution
             @series begin
-                #color
+                #Set color
                 color := prior_color
-                #transparency
+                #Set transparency
                 fill := (0, 0.5)
-                #subplot nr
-                subplot := i
+                #Set subplot nr
+                subplot := plot_number
                 #Only put legend on first plot
-                if i != 1
+                if plot_number != 1
                     legend := nothing
                 end
-                #Empty labels
+                #Remove labels
                 label := nothing
-                #Get the distribution to plot it
-                params_prior_list[Symbol(param_names[i])]
+                #Plot the distribution
+                param_prior
             end
 
-            #Plot the posterior
+            ## Posterior distribution
             @series begin
-                #This is a density (not a functional form)
+                #This is a density (not a functional form like the prior)
                 seriestype := :density
 
+                #Set color
                 color := posterior_color
+                #Set transparency
                 fill := (0, 0.5)
-                subplot := i
-                if i != 1
+                #Set subplot number
+                subplot := plot_number
+                #Only show legend on the first plot
+                if plot_number != 1
                     legend := nothing
                 end
+                #Remove label
+                label := nothing
+                #Plot the posterior
+                param_posterior
+            end
+        end
+
+        ### Plot uncertainty intervals ###
+        if show_intervals
+
+            ## Prior outer interval
+            @series begin
+                #A scatterplot errorbar will be used to show the interval
+                seriestype := :scatter
+                #Set color
+                color := prior_color
+                #Subplot number
+                subplot := plot_number
+                #Only show legend on first plot
+                if plot_number != 1
+                    legend := nothing
+                end
+                #Remove labels
                 label := nothing
 
-                #Make an array of samples for the specified parameter to plot it
-                Array(chain[:, param_names[i], :])[:]
+                #Set a low line thickness
+                markerstrokewidth := 1
+                #Set color of the bar
+                markerstrokecolor := prior_color
+
+                #Set the size of the errorbar
+                xerror := ([prior_outer_interval_lower], [prior_outer_interval_upper])
+
+                #Plot the point in order to get the errorbar
+                [(prior_median, prior_interval_offset)]
+            end
+
+            ## Prior inner interval
+            @series begin
+                #A scatterplot errorbar will be used to show the interval
+                seriestype := :scatter
+                #Set color
+                color := prior_color
+                #Subplot number
+                subplot := plot_number
+                #Only show legend on first plot
+                if plot_number != 1
+                    legend := nothing
+                end
+                #Remove labels
+                label := nothing
+
+                #Set a higher line thickness
+                markerstrokewidth := 3
+                #Set color of the bar
+                markerstrokecolor := prior_color
+
+                #Set the size of the errorbar
+                xerror := ([prior_inner_interval_lower], [prior_inner_interval_upper])
+
+                #Plot the point in order to get the errorbar
+                [(prior_median, prior_interval_offset)]
+            end
+
+
+            ## Posterior outer interval
+            @series begin
+                #A scatterplot errorbar will be used to show the interval
+                seriestype := :scatter
+                #Set color
+                color := posterior_color
+                #Subplot number
+                subplot := plot_number
+                #Only show legend on first plot
+                if plot_number != 1
+                    legend := nothing
+                end
+                #Remove labels
+                label := nothing
+
+                #Set a low line thickness
+                markerstrokewidth := 1
+                #Set color of the bar
+                markerstrokecolor := posterior_color
+
+                #Set the size of the errorbar
+                xerror :=
+                    ([posterior_outer_interval_lower], [posterior_outer_interval_upper])
+
+                #Plot the point in order to get the errorbar
+                [(posterior_median, posterior_interval_offset)]
+            end
+
+            ## Posterior inner interval
+            @series begin
+                #A scatterplot errorbar will be used to show the interval
+                seriestype := :scatter
+                #Set color
+                color := posterior_color
+                #Subplot number
+                subplot := plot_number
+                #Only show legend on first plot
+                if plot_number != 1
+                    legend := nothing
+                end
+                #Remove labels
+                label := nothing
+
+                #Set a low line thickness
+                markerstrokewidth := 3
+                #Set color of the bar
+                markerstrokecolor := posterior_color
+
+                #Set the size of the errorbar
+                xerror :=
+                    ([posterior_inner_interval_lower], [posterior_inner_interval_upper])
+
+                #Plot the point in order to get the errorbar
+                [(posterior_median, posterior_interval_offset)]
             end
         end
 
-
-        ### Plot prior median and errorbar ###
+        ### Plot medians ###
+        ## Prior median
         @series begin
+            #Scatterplot or a single point
             seriestype := :scatter
+            #Set color
             color := prior_color
-            subplot := i
-
-            #Set size of big errorbar
-            xerror := ([prior_outer_interval_lower[i]], [prior_outer_interval_upper[i]])
-
-            markerstrokewidth := 1 #thickness
-            markerstrokecolor := prior_color
-            if i != 1
+            #Subplot number
+            subplot := plot_number
+            #Only show legend for first plot
+            if plot_number != 1
                 legend := nothing
             end
-            label := nothing
-            #Need to plot the point in order to make the rrorbar
-            [(prior_median[i], prior_interval_offset)]
-        end
-        #plot small errorbar
-        @series begin
-            seriestype := :scatter
-            color := prior_color
-            subplot := i
-            xerror := ([prior_inner_interval_lower[i]], [prior_inner_interval_upper[i]])
-            markerstrokewidth := 3 #thickness
-            markerstrokecolor := prior_color
-            if i != 1
-                legend := nothing
-            end
-            label := nothing
-            [(prior_median[i], prior_interval_offset)]
-        end
 
-        #Plot point
-        @series begin
-            seriestype := :scatter
-            color := prior_color
-            subplot := i
+            #Point size
             markerstrokewidth := 1
             markersize := 5
-            if i != 1
-                legend := nothing
-            end
+
+            #Set label to prior
             label := "Prior"
-            [(prior_median[i], prior_interval_offset)]
+
+            #Plot the prior median
+            [(prior_median, prior_interval_offset)]
         end
 
-
-
-        ### Plot posterior median and errorbar ###
+        ## Posterior
         @series begin
+            #Scatterplot or a single point
             seriestype := :scatter
+            #Set color
             color := posterior_color
-            subplot := i
-            xerror := ([posterior_outer_interval_lower[i]], [posterior_outer_interval_upper[i]])
+            #Subplot number
+            subplot := plot_number
+            #Only show legend for first plot
+            if plot_number != 1
+                legend := nothing
+            end
+
+            #Point size
             markerstrokewidth := 1
-            markerstrokecolor := posterior_color
-            if i != 1
-                legend := nothing
-            end
-            label := nothing
-            [(posterior_median[i], posterior_interval_offset)]
-        end
-        @series begin
-            seriestype := :scatter
-            color := posterior_color
-            subplot := i
-            xerror := ([posterior_inner_interval_lower[i]], [posterior_inner_interval_upper[i]])
-            markerstrokewidth := 3
-            markerstrokecolor := posterior_color
-            if i != 1
-                legend := nothing
-            end
-            label := nothing
-            [(posterior_median[i], posterior_interval_offset)]
-        end
-        @series begin
-            seriestype := :scatter
-            color := posterior_color
             markersize := 5
-            subplot := i
-            markerstrokewidth := 1
-            if i != 1
-                legend := nothing
-            end
+
+            #Set label to prior
             label := "Posterior"
-            [(posterior_median[i], posterior_interval_offset)]
+
+            #Plot the prior median
+            [(posterior_median, posterior_interval_offset)]
         end
     end
 end
