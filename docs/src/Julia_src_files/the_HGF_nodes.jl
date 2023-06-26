ad6d666aa40ebb428879385f5a21d6bbf325f4fe
Anna280 <54855627+Anna280@users.noreply.github.com>
2023-06-26 13:00:39 +0200

    updated svg files correct scaling

--- docs/src/Julia_src_files/the_HGF_nodes.jl
+++ docs/src/Julia_src_files/the_HGF_nodes.jl
@@ -2,126 +2,21 @@
 
 # In this section we will cover the types of nodes, their parameters and the rules for structuring your own HGF. 
 
-
 # ### Overview
 
-#   - [The Node Types In an HGF and their states](#The-Node-Types-In-an-HGF)
-#   - [Building principles ](#Building-principles )
+# ![states and parameters](../images/states_nodes/all_states.svg)
 
-# ### The Node Types In an HGF
+# ## Building principles
 
-# We have six types of nodes in the HGF: binary (state node and input node), categorical (state node and input node), and continuous (state node and input node).
-
-# If a node's parameters are configured with a default value, they are stated below as well. 
-
-# We provide an overview of the states in each of the nodes. 
-
-# 1. [Binary Nodes](#Binary-Nodes)
-#     1. [State Node: States and parameters](#The-states-of-binary-state-nodes-and-parameters)
-#     2. [Input Node: States and parameters](#The-states-of-binary-input-nodes-and-parameters)
-
-# 3. [Categorical Nodes](#Categorical-Nodes)
-#    1. [State Node: States and parameters](#The-states-of-Categorical-state-nodes-and-parameters)
-#    2. [Input Node: States and parameters](#The-states-of-Categorical-Input-nodes-and-parameters)
-
-# 4. [Continuous Nodes](#Continuous-Nodes)
-#    1. [State Node: States and parameters](#The-states-of-Continuous-state-nodes-and-parameters)
-#    2. [Input Node: States and parameters](#The-states-of-Continuous-Input-nodes-and-parameters)
-
-
-# ## Binary Nodes
-
-# ### The states of binary state nodes
-
-# - posterior_mean
-# - posterior_precision
-# - value\_prediction\_error
-# - prediction_mean
-# - prediction_precision
-
-# ### Parameters
-
-# - Value coupling
-
-
-# ### The states of binary input nodes and parameters
-
-# - input value
-# - value prediction error
-
-# ### Parameters
-
-# - Category means (default is [0,1])
-# - Input precision (default is  infinite input precision)
-
-
-# ## Categorical Nodes
-
-# ### The states of Categorical state nodes and parameters
-
-# - posterior
-# - value prediction error
-# - prediction
-
-# ### Parameters
-
-# - no parameters in the categorical state node
-
-# ### The states of Categorical input nodes and parameters
-
-# - input value
-
-# ### Parameters
-
-# - no parameters in the categorical state node
-
-# ## Continuous Nodes
-
-# ### The states of Continuous state nodes and parameters
-
-# #### States
-
-# - posterior mean
-# - posterior precision
-# - value prediction error
-# - volatility prediction error
-# - prediction mean
-# - prediciton volatility
-# - prediction precision
-# - auxiliary prediction precision
-
-# ### Parameters
-
-# - evolution rate (default is 0)
-# - value coupling
-# - volatility coupling
-# - initial mean (default is 0)
-# - initital precision (default is 0)
-
-
-# ### The states of Continuous input nodes and parameters
-
-# - input value
-# - value prediction error
-# - volatility prediction error
-# - prediction volatility
-# - prediction precision
-
-# ### Parameters
-
-# - evolution rate (default is 0)
-# - value coupling 
-# - volatility coupling
- 
-
-
-# ## Building principles 
+# The following rules apply for connecting nodes, when customizing your own HGF structure:
 
 # ### Binary state node rules:
  
 # - Can only have exactly one value parent
 # - Can only have excatly one value child
 # - Can only have a contionus state node as value parent
+
+# ![states and parameters](../images/states_nodes/binary_nodes.svg)
 
 # ### continuous state node rules:
 
@@ -132,8 +27,12 @@
 # - Can’t have the same value parent as volatility parent
 # - Can’t have the same value child as volatility child
 
+# ![states and parameters](../images/states_nodes/continuous_nodes.svg)
+
 # ### Categorical state node rules:
 
 # - Can only have exactly one value child
 # - Can only have categorical input node as child
 # - Can only have binary state node as parents
+
+# ![states and parameters](../images/states_nodes/categorical_nodes.svg)
